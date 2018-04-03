2d3c5b9f5b45a37fb81b92a9f81cf71ee9944a66
Garnett Puett and Matt Nardoni <garnett@>
2018-04-03 13:48:01 -0700

    Fix javascript functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 // business logic
-function Ticket(time, movie) {
+function Ticket(time, movie, age) {
   this.time = time;
   this.movie = movie;
+  this.age = age;
 }
 
 Ticket.prototype.price = function() {
-  if (age < 17 && movie === "Blade Runner 2049") {
-    return ("No Entry")
+  var price = "";
+  if (this.movie === "Blade Runner 2049" && this.age < 17) {
+    return price = "unavailable due to your age, kiddo"
+  } else if (this.time === "3:00pm" || this.age >= 65) {
+    return price = "$6";
+  } else if (this.movie === "Tommy Boy") {
+    return price = "$8";
+  } else {
+    return price = "$12";
   }
+  return price;
 }
 
 // user interface logic
@@ -19,9 +28,9 @@ $(function() {
     var inputTime = $("input:radio[name=time]:checked").val();
     var inputAge = parseInt($("#age").val());
     var inputMovie = $("#movie").val();
-    var newTicket = new Ticket (inputTime, inputMovie);
-    console.log(newTicket);
-    $(".price").text(Ticket.price());
+    var newTicket = new Ticket (inputTime, inputMovie, inputAge);
+    var prices = newTicket.price();
+    $(".price").text(newTicket.movie + " at " + newTicket.time + " will be " + prices + ".");
     $("#output").show();
   });
 });
